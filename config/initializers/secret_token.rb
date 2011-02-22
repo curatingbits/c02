e348ebb1297badcefb11930a60e014b998bbf4a6
Duggan Roberts <droberts@quailcreekbank.com>
2011-02-21 22:28:56 -0600

    and so it begins.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C02::Application.config.secret_token = '79c5a8dfe966e1cde77aa971d060a6c487ef4cd556c3781387b69f4821eb57ce9002dd8984af81da18e72eeb9bba6b4204be41d25ccd04aecc6c68ed47b2b09d'
